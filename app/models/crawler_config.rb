700621981bb2b2765231289a12a7a80fc0126e1f
Alessian Dormal <alessian@alessian.be>
2025-06-21 06:49:29 +0200

    fix mailer using the model class instead of active storage

--- app/models/crawler_config.rb
+++ app/models/crawler_config.rb
@@ -1,2 +1,11 @@
 class CrawlerConfig < ApplicationRecord
+  # Always give me “the one row”
+  class << self
+    def current = first_or_create!
+
+    delegate :sender_email,
+             :time_of_day,
+            :regularity_days,
+  end
 end
+
