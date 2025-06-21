08fe5ee7dc45f7bb6c40d0f1db1b694406605aff
Alessian Dormal <alessian@alessian.be>
2025-06-21 06:51:13 +0200

    forgot an end

--- app/models/crawler_config.rb
+++ app/models/crawler_config.rb
@@ -1,11 +1,12 @@
 class CrawlerConfig < ApplicationRecord
   # Always give me “the one row”
-  class << self
-    def current = first_or_create!
+    class << self
+        def current = first_or_create!
 
-    delegate :sender_email,
-             :time_of_day,
-            :regularity_days,
-  end
+        delegate :sender_email,
+                :time_of_day,
+                :regularity_days,
+        end
+    end
 end
 
