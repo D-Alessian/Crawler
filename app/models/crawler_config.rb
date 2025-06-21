a7800802d2405be790d0eed84b201c0b7ae11c8c
Alessian Dormal <alessian@alessian.be>
2025-06-21 06:53:19 +0200

    forgot a comma

--- app/models/crawler_config.rb
+++ app/models/crawler_config.rb
@@ -1,12 +1,10 @@
 class CrawlerConfig < ApplicationRecord
-  # Always give me “the one row”
     class << self
         def current = first_or_create!
 
         delegate :sender_email,
                 :time_of_day,
-                :regularity_days,
-        end
+                :regularity_days
     end
 end
 
