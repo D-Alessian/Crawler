abb3af72218bc1784d58be94b8e885a2c62444ab
Alessian Dormal <alessian@alessian.be>
2025-06-23 12:45:52 +0200

    tweak the importmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,8 +5,8 @@ pin '@hotwired/turbo-rails', to: 'turbo.min.js'
 pin '@hotwired/stimulus', to: 'stimulus.min.js'
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
 pin_all_from 'app/javascript/controllers', under: 'controllers'
+pin "bootstrap", to: "bootstrap.min.js", preload: true
+pin "@popperjs/core", to: "popper.js", preload: true
 pin "jquery" # @3.7.1
 pin "cocoon" # @0.1.1
-pin "bootstrap" # @5.3.7
-pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
 pin "cocoon-js" # @0.0.5
