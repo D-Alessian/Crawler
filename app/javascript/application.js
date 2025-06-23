6d3f74238fc5cd546b2adc4d714fb1a130dc416f
Alessian Dormal <alessian@alessian.be>
2025-06-23 13:07:05 +0200

    change the application.js to use relative path for controllers

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
-import "controllers"
-
+import "./controllers"
+import "@popperjs/core"
 import "bootstrap"
-
-import "jquery"
-import "cocoon-js"
-
-window.$ = window.jQuery = require("jquery")
